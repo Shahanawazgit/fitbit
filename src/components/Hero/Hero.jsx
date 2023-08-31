85deb240476b0bc24b8515191976eceb2be8e0fa
Shahanawaz <shahanawazkhan579@gmail.com>
2023-08-31 23:06:53 +0530

    Made Folder structure & header

---
+++ src/components/Hero/Hero.jsx
@@ -0,0 +1,63 @@
+import React from "react";
+import "./Hero.css";
+import Header from "../Header/Header";
+
+const Hero = () => {
+  return (
+    <div className="hero">
+      <div className="left-h">
+        <Header />
+
+        {/* the best ad part */}
+        <div className="the-best-ad">
+          <div></div>
+          <span>the best fitness club in the town</span>
+        </div>
+
+        {/* Hero Heading */}
+        <div className="hero-text">
+          <div>
+            <span className="stroke-text">shape </span>
+            <span>your</span>
+          </div>
+          <div>
+            <span>ideal body</span>
+          </div>
+
+          <div>
+            <span>
+              In here we will help you to shape and build your ideal body and
+              live up your life to fullest
+            </span>
+          </div>
+        </div>
+
+        {/* Figures */}
+        <div className="figures">
+          <div>
+            <span>+140</span>
+            <span>expert coachs</span>
+          </div>
+          <div>
+            <span>+978</span>
+            <span>members joined</span>
+          </div>
+          <div>
+            <span>+50</span>
+            <span>fitness programs</span>
+          </div>
+        </div>
+
+        {/* Hero Buttons */}
+        <div className="hero-buttons">
+          <button className="btn">Get Started</button>
+          <button className="btn">Learn More</button>
+        </div>
+      </div>
+
+      <div className="right-h">right side</div>
+    </div>
+  );
+};
+
+export default Hero;
